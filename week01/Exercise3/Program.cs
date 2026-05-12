9529740b34c114006d403fd60e1a39d56c20ff0d
¨marypazdesigner0412¨ <ldehernandez@byupathway.edu>
2026-05-12 01:54:00 -0400

    Completed exercise 3, loops.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string keepPlaying = "yes";
+
+        // This outer loop handles the "Play Again" logic
+        do
+        {
+            // Set up the random magic number
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 0; // Stretch: Variable to track attempts
+
+            // This inner loop handles the guessing process
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++; // Increment the counter each time a guess is made
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    // Stretch: Inform them of the total guesses
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                }
+            }
+
+            // Stretch: Ask to play again
+            Console.Write("Do you want to play again (yes/no)? ");
+            keepPlaying = Console.ReadLine().ToLower();
+
+        } while (keepPlaying == "yes");
+
+        Console.WriteLine("Goodbye!");
     }
 }
